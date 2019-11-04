2d4f7c7ef9c0c933963a985f5b685ea4bf01e0af
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-11-04 00:02:43 -0800

    adding necessary fields

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userModel = new Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  wallet: Integer,
-  bet: Integer,
-  user_ref: String,
-  event: String,
-  chosenTeam: String
-});
+const userModel = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    eventDate: { type: Date },
+    wallet: Number,
+    bet: Integer,
+    user_ref: { type: String },
+    event: { type: String },
+    selection: { type: String },
+    result: { type: String, default: "Pending" },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model("User", userModel);
 
