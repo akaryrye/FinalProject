bfbf20ca3c67c5ff372f54a38b949f05b05a2303
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-11-03 23:49:32 -0800

    added db models

--- models/userModel.js
+++ models/userModel.js
@@ -4,8 +4,14 @@ const Schema = mongoose.Schema;
 const userModel = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  name: { type: String, required: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  wallet: Integer,
+  bet: Integer,
+  user_ref: String,
+  event: String,
+  chosenTeam: String
 });
 
 const User = mongoose.model("User", userModel);
