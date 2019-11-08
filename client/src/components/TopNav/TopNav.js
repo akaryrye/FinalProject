da8071c4423286fcfc6894fcf62910f86cba9daf
unknown <h.e.ergaster@gmail.com>
2019-11-07 19:05:54 -0800

    navbar layout

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Nav, NavItem, NavLink } from "reactstrap";
+import "./TopNav.css";
 
 const TopNav = props => {
   return (
     <div>
       <Nav>
-        <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink>{" "}
-        <NavLink href="#">Another Link</NavLink>{" "}
-        <NavLink disabled href="#">
-          Disabled Link
-        </NavLink>
+        <NavLink href="#" id="Home">
+          Logo
+        </NavLink>{" "}
+        <NavLink href="#" id="aboutUs">
+          About Us
+        </NavLink>{" "}
+        <NavLink href="#" id="wallet">
+          Wallet
+        </NavLink>{" "}
+        <NavLink href="#" id="shop">
+          Shop
+        </NavLink>{" "}
+        <div id="authentication">
+          <NavLink href="#" id="signUp">
+            Sign Up
+          </NavLink>
+          <NavLink href="#" id="login">
+            Login
+          </NavLink>
+        </div>
       </Nav>
     </div>
   );
