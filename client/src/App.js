070952fc9a1c46145102dfd38f3d7f3692b92357
unknown <h.e.ergaster@gmail.com>
2019-11-14 20:32:22 -0800

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import TopNav from "./Components/TopNav/TopNav";
 import StreamBox from "./Components/Carousel/Carousel";
 import Chat from "./Components/Chat/Chat";
@@ -7,16 +7,12 @@ import Stream from "./Components/Stream";
 import Auth from "./Components/Auth";
 import "./base.css";
 
-
 function App() {
   return (
     <Router>
       <div>
         <TopNav />
-        <StreamBox />
         <Stream />
-        <Chat />
-        <Auth />
       </div>
     </Router>
   );
